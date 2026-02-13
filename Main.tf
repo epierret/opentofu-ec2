dc64c84f84c1127a3094564fbe89f4c65c98da9e
Enrique Pierret <pierretenrique@gmail.com>
2026-02-13 13:37:04 +0100

    Edit Main.tf

--- Main.tf
+++ Main.tf
@@ -1,3 +1,15 @@
-Provider "aws" { region     = "us-east-1" access_key = "" secret_key = "x" } resource "aws_instance" "opentofuvm" { ami           = "ami-053b0d53c279acc90" instance_type = "t3.micro"
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "YOUR_ACCESS_KEY"   # À remplir
+  secret_key = "YOUR_SECRET_KEY"   # À remplir
+}
 
-Subnet_id = "subnet-xxxxxxxxxx"
+resource "aws_instance" "opentofuvm" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t3.micro"
+  subnet_id     = "subnet-xxxxxxxxxx"  # Remplace par ton subnet réel
+
+  tags = {
+    Name = "OpenTofuVM"
+  }
+}
