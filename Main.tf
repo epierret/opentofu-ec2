64073a2752ce6ba215f1e3b1908869d078fc3fe6
Enrique Pierret <pierretenrique@gmail.com>
2026-02-16 23:31:38 +0100

    Remove comments from AWS provider access keys

--- Main.tf
+++ Main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "YOUR_ACCESS_KEY"   # À remplir
-  secret_key = "YOUR_SECRET_KEY"   # À remplir
+  access_key = "YOUR_ACCESS_KEY"   
+  secret_key = "YOUR_SECRET_KEY"   
 }
 
 resource "aws_instance" "opentofuvm" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t3.micro"
-  subnet_id     = "subnet-xxxxxxxxxx"  # Remplace par ton subnet réel
+  subnet_id     = "subnet-xxxxxxxxxx"  
 
   tags = {
     Name = "OpenTofuVM"
