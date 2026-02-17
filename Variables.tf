34ba5f3db4df105193df80ddca2f604da06e060c
Enrique Pierret <pierretenrique@gmail.com>
2026-02-17 15:08:19 +0100

    Define aws_region variable in Variables.tf
    Added variable for AWS region with default value.

--- Variables.tf
+++ Variables.tf
@@ -1,3 +1,5 @@
-provider "aws" {  region     = "us-east-1"  access_key = "YOUR_ACCESS_KEY"secret_key = "YOUR_SECRET_KEY"   # À remplir}
-resource "aws_instance" "opentofuvm" {  ami           = "ami-053b0d53c279acc90"  instance_type = "t3.micro"  subnet_id     = "subnet-xxxxxxxxxx"  # Remplace par ton subnet réel
-  tags = {    Name = "OpenTofuVM"  }}
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
